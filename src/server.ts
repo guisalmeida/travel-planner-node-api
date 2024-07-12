c57e6a55789eda0e0e8e69eeb250ce1df6dc184b
guisalmeida <guisalmeida.dev@gmail.com>
2024-07-11 21:21:30 -0300

    chore: add cors

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,13 @@ import fastify from 'fastify';
 import { createTrip } from './routes/create-trip';
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
 import { confirmTrip } from './routes/confirm-trip';
+import cors from '@fastify/cors';
 
 const app = fastify();
+
+app.register(cors, {
+  origin: '*'
+})
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
