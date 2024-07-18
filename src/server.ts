d6d9fa3cf8ffa4f49b86364ce881d046a1ac79bf
guisalmeida <guisalmeida.dev@gmail.com>
2024-07-18 19:45:22 -0300

    feat: create links routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import fastify from 'fastify';
+import cors from '@fastify/cors';
 import { createTrip } from './routes/create-trip';
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
 import { confirmTrip } from './routes/confirm-trip';
-import cors from '@fastify/cors';
 import { confirmParticipant } from './routes/confirm-participant';
 import { createActivity } from './routes/create-activity';
 import { getActivities } from './routes/get-activities';
+import { createLink } from './routes/create-link';
+import { getLinks } from './routes/get-links';
 
 const app = fastify();
 
@@ -17,10 +19,12 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
+app.register(createActivity);
+app.register(createLink);
 app.register(confirmTrip);
 app.register(confirmParticipant);
-app.register(createActivity);
 app.register(getActivities);
+app.register(getLinks);
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server running...');
